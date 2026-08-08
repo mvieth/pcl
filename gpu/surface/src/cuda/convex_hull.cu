b1ea1f7276d8ef4e7dff0684f30c0da184367f29
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2026-08-08 15:55:42 +0200

    Thrust deprecated

--- gpu/surface/src/cuda/convex_hull.cu
+++ gpu/surface/src/cuda/convex_hull.cu
@@ -137,7 +137,11 @@ namespace pcl
       int transform_reduce_index(It beg, It end, Unary unop, Init init, Binary binary)
 	  {
 	    thrust::counting_iterator<int> cbeg(0);
+#if THRUST_VERSION >= 300100
+		thrust::counting_iterator<int> cend = cbeg + cuda::std::distance(beg, end);
+#else
 		thrust::counting_iterator<int> cend = cbeg + thrust::distance(beg, end);
+#endif
 
 	    thrust::tuple<float, int> t = thrust::transform_reduce(
 		  thrust::make_zip_iterator(thrust::make_tuple(beg, cbeg)),
